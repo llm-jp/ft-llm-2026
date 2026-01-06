fc8a6716f41f8754bf312dae96f7878600cc243c
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2026-01-06 13:32:37 +0900

    apply formatter

--- math-eval/tests/test_strict.py
+++ math-eval/tests/test_strict.py
@@ -2,28 +2,23 @@ import pytest
 
 from math_eval.main import parse_and_verify
 
+
 @pytest.mark.parametrize(
     "prediction, gold, expected",
     [
         # expansion
-        (r"$(x+1)^2$", r"$x^2+2x+1$", False), 
-        (r"$1 + 2x + x^2$", r"$x^2+2x+1$", True), 
-
+        (r"$(x+1)^2$", r"$x^2+2x+1$", False),
+        (r"$1 + 2x + x^2$", r"$x^2+2x+1$", True),
         # calculation
-        (r"$\sqrt{2}^2$", r"$2$", False), 
+        (r"$\sqrt{2}^2$", r"$2$", False),
         (r"$5a-8a$", r"$-3a$", False),
         (r"$1+2$", r"$3$", False),
-
         # division
-        (r"$6/2$", r"$3/1$", True), 
-
+        (r"$6/2$", r"$3/1$", True),
         # different expression
-        (r"$2\sqrt{2}$", r"$2^{3/2}$", False), 
+        (r"$2\sqrt{2}$", r"$2^{3/2}$", False),
     ],
 )
-
 def test_verify(prediction: str, gold: str, expected: bool) -> None:
     result = parse_and_verify(prediction, gold, evaluation_method="strict")
     assert result == expected
-
-
